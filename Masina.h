1ec999e726072ae9914a1bffcc6a6838eb2dd4f0
Andrei <andreivirgil@hotmail.com>
2022-03-15 18:05:46 +0200

    Primul Update

--- Masina.h
+++ Masina.h
@@ -13,6 +13,8 @@ private:
     int probleme[5];
     int manopera;
     int anFabricatie;
+    int CaiPutere;
+
 public:
     Masina(string marca, string model, int anFabricatie, string culoare, bool reparata, int probleme[])
     {
@@ -21,18 +23,34 @@ public:
         this->anFabricatie = anFabricatie;
         this->culoare = culoare;
         this->reparata = reparata;
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i <= 5; i++)
             this->probleme[i] = probleme[i];
+        this->CalculManopera();
     }
+
     Masina()
     {
         this->marca = "Inexistenta";
         this->model = "";
         this->culoare = "";
         this->reparata = "Inexistenta";
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i <= 5; i++)
             this->probleme[i] = -1;
     }
+
+    Masina(Masina& aux)
+    {
+        this->marca = aux.marca;
+        this->model = aux.model;
+        this->culoare = aux.culoare;
+        this->reparata = aux.reparata;
+        for (int i = 0; i <= 5; i++)
+            this->probleme[i] = aux.probleme[i];
+        this->manopera = aux.manopera;
+        this->anFabricatie = aux.anFabricatie;
+        this->CaiPutere = aux.CaiPutere;
+    }
+
     friend istream& operator>>(istream& in, Masina& m)
     {
         //string marca, model, culoare;
@@ -46,8 +64,155 @@ public:
         in>>m.culoare;
         cout<<"Introduceti anul fabriecatiei masinii"<<endl;
         in>>m.anFabricatie;
-        //cout<<""
+        cout<<"Introduceti caii putere ai masinii"<<endl;
+        in>>m.CaiPutere;
+        cout<<"Alegeti serviciile dorite: "<<endl<<"1. Reparatie frane"<<endl<<"2. Schimb ulei"<<endl<<"3. Reparatie climatizare"<<endl<<"4. Tinichigerie"<<endl<<"5. ITP"<<endl<<"6. Soft arabesc"<<endl<<"0. Toate serviciile au fost adaugate"<<endl;
+        int codProblema;
+        in>> codProblema;
+        for (int i = 0; i<=5;i++)
+            m.probleme[i] = 0;
+        while (codProblema != 0)
+        {
+            m.probleme[codProblema - 1] = 1;
+            cout<<endl<<"Problema Adaugata"<<endl;
+            in>>codProblema;
+        }
+        m.reparata = false;
+        m.CalculManopera();
         return in;
+    }
+
+    friend ostream& operator<<(ostream& out, Masina& m)
+    {
+        if (m.reparata == false)
+        {
+            cout<<endl<<"Pentru masina "<<m.marca<<" "<<m.model<<" de culoare "<<m.culoare<<", din anul "<<m.anFabricatie<<" cu "<<m.CaiPutere<<" CP, se ofera urmatoarele servicii:"<<endl;
+            if (m.probleme[0] == 1)
+                cout<<"Reparatie frane"<<endl;
+            if (m.probleme[1] == 1)
+                cout<<"Schimb ulei"<<endl;
+            if (m.probleme[2] == 1)
+                cout<<"Reparatie climatizarea"<<endl;
+            if (m.probleme[3] == 1)
+                cout<<"Tinichigerie"<<endl;
+            if (m.probleme[4] == 1)
+                cout<<"ITP"<<endl;
+            if (m.probleme[5] == 1)
+                cout<<"Soft arabesc"<<endl;
+            cout<<"Costul total fiind: "<<m.manopera<<" lei";
+        }
+        else
+        {
+            cout<<endl<<"Masina "<<m.marca<<" "<<m.model<<" de culoare "<<m.culoare<<", din anul "<<m.anFabricatie<<" cu "<<m.CaiPutere<<" CP, a fost reparata cu succes, iar costul total fiind de: "<<m.manopera<<" lei";
+        }
+        return out;
+    }
+
+    Masina& operator=(const Masina& aux)
+    {
+        marca = aux.marca;
+        model = aux.model;
+        culoare = aux.culoare;
+        reparata = aux.reparata;
+        for (int i = 0; i <= 5; i++)
+            probleme[i] = aux.probleme[i];
+        manopera = aux.manopera;
+        anFabricatie = aux.anFabricatie;
+        CaiPutere = aux.CaiPutere;
+        return *this;
+    }
+
+    #pragma region "Getteri si Setteri"
+    const string &getMarca() const {
+        return marca;
+    }
+
+    void setMarca(const string &marca) {
+        Masina::marca = marca;
+    }
+
+    const string &getModel() const {
+        return model;
+    }
+
+    void setModel(const string &model) {
+        Masina::model = model;
+    }
+
+    const string &getCuloare() const {
+        return culoare;
+    }
+
+    void setCuloare(const string &culoare) {
+        Masina::culoare = culoare;
+    }
+
+    bool isReparata() const {
+        return reparata;
+    }
+
+    void setReparata(bool reparata) {
+        Masina::reparata = reparata;
+    }
+
+    const int *getProbleme() const {
+        return probleme;
+    }
+
+    int getManopera() const {
+        return manopera;
+    }
+
+    void setManopera(int manopera) {
+        Masina::manopera = manopera;
+    }
+
+    int getAnFabricatie() const {
+        return anFabricatie;
+    }
+
+    void setAnFabricatie(int anFabricatie) {
+        Masina::anFabricatie = anFabricatie;
+    }
+
+    int getCaiPutere() const {
+        return CaiPutere;
+    }
+
+    void setCaiPutere(int caiPutere) {
+        CaiPutere = caiPutere;
+    }
+
+#pragma endregion
+
+    void CalculManopera()
+    {
+        int sumaTotala = 0;
+        if (this->probleme[0] == 1)
+            sumaTotala += 1000;
+        if (this->probleme[1] == 1)
+            sumaTotala += 100;
+        if (this->probleme[2] == 1)
+            sumaTotala += 500;
+        if (this->probleme[3] == 1)
+            sumaTotala += 2000;
+        if (this->probleme[4] == 1)
+            sumaTotala += 250;
+        if (this->probleme[5] == 1)
+            sumaTotala += 1500;
+        this->manopera = sumaTotala;
+    }
+
+    void SoftArabesc()
+    {
+        cout<<endl<<"Cati caluti ii bagam verik?"<<endl;
+        int bonus;
+        cin>>bonus;
+        this->CaiPutere += bonus;
+    }
+
+    ~Masina()
+    {
 
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
+#include <stdlib.h>
 #include "Masina.h"
 using namespace std;
 
+int menu()
+{
+    system("cls");
+    cout<<"Bine ati venit la Vericu Service! "<<endl;
+    cout<<"1. Adaugati o masina noua in service"<<endl;
+    cout<<"2. Vizualizati masinile aflate momentan in service"<<endl;
+    cout<<"3. Vizualizati masinile reparate de service-ul nostru"<<endl;
+    cout<<"4. Modificare servicii ale unei masini"<<endl;
+    cout<<"0. Exit"<<endl;
+    int optiune;
+    cin>>optiune;
+    while (optiune > 4)
+    {
+        cout<<endl<<"Optiune introdusa inexistenta!"<<endl;
+        cin>>optiune;
+    }
+    return optiune;
+}
+
 int main() {
-    Masina m1;
-    cin>>m1;
+    //Masina m1;
+    //cin>>m1;
+    //Masina m2 = m1;
+    //cout<<m2;
+    Masina *masiniService;
+    int nrMasiniService = 0, nrMasiniReparate = 0;
+    Masina *masiniReparate;
+    int optiune = menu();
+    while (optiune != 0)
+    {
+        switch (optiune)
+        {
+            case 1:
+            {
+                system("CLS");
+                //Masina aux;
+                //cin>>aux;
+                //nrMasiniService++;
+                //masiniService = new Masina[nrMasiniService+1];
+                //masiniService[nrMasiniService] = aux;
+                //cout<<masiniService[nrMasiniService];
+                nrMasiniService++;
+                masiniService = new Masina[nrMasiniService];
+                cin>>masiniService[nrMasiniService -1];
+                cout<<masiniService[nrMasiniService -1];
+                break;
+            }
+        }
+    }
     return 0;
 }
